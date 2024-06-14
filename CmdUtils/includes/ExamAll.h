749a34081fcb2c0e33848dd14e4ad5e16f9035e8
liheng <159103134+lmliheng@users.noreply.github.com>
2024-06-14 14:51:45 +0800

    Delete CmdUtils directory

--- CmdUtils/includes/ExamAll.h
+++
@@ -1,6 +0,0 @@
-#ifndef EXAMALL_H
-#define EXAMALL_H
-
-void login_scripts(const char *script);
-
-#endif // EXAMALL_H
--- CmdUtils/includes/FileInfo.h
+++
@@ -1,6 +0,0 @@
-#ifndef FILEINFO_H
-#define FILEINFO_H
-
-void PrintFileInfo(const char *script);
-
-#endif // FILEINFO_H
