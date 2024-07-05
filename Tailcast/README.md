f249d7eeec712b02dc49f0d4bd0fb3947d986971
lmliheng <liheng2137@qq.com>
2024-07-05 15:42:21 +0800

    更新产品文档

---
+++ Tailcast/README.md
@@ -0,0 +1,39 @@
+# Tailcast
+
+Dark themed website template built on AstroJS, designed for fictional startup
+
+## Tech stack:
+
+Astro, React, Tailwind, Framer Motion
+
+## Live link
+[https://tailcast.vercel.app/](https://tailcast.vercel.app/)
+
+##  Project Structure
+
+```
+├── public/
+│   └── favicon.svg
+├── src/
+│   ├── assets
+│   │   ├── icons
+│   │   ├── images
+│   │   └── logos
+│   ├── components
+│   ├── layouts
+│   ├── pages
+│   └── styles
+└── package.json
+```
+
+##  How to run
+
+All commands are run from the root of the project, from a terminal:
+
+| Command                | Action                                             |
+| :--------------------- | :------------------------------------------------- |
+| `npm install`          | Installs dependencies                              |
+| `npm run dev`          | Starts local dev server at `localhost:3000`        |
+| `npm run build`        | Build your production site to `./dist/`            |
+| `npm run preview`      | Preview your build locally, before deploying       |
+
---
+++ Tailcast/tailwind.config.cjs
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "rgb(99, 102, 241)",
+        secondaryColor: "rgb(161, 163, 247)",
+        primaryText: "rgb(255,255,255)",
+        secondaryText: "rgb(174, 178, 183)",
+        bgDark1: "rgb(31, 32, 35)",
+        bgDark2: "rgb(38, 39, 43)",
+        bgDark3: "rgb(48, 49, 54)",
+        bgDark3Hover: "rgb(55, 56, 62)",
+        bgDarkTransparent: "rgb(31, 32, 35, 0.7)",
+        bgDarkTransparentDarker: "rgb(0,0,0,0.5)",
+        bgDarkTransparentLighter: "rgb(48, 49, 54, 0.7)",
+        mainBorder: "rgb(255,255,255,0.15)",
+        mainBorderDarker: "rgb(255,255,255,0.07)",
+        quoteIconColor: "rgb(178, 184, 205)"
+      },
+      fontFamily: {
+        Inter: "Inter",
+      },
+      screens: {
+        xs: "530px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",      
+        "2xl": "1536px",
+      },
+    },
+  },
+};
+
